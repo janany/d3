012170b01372f6163d33c142ac56abdda09f880c
jananij <janani.j@imaginea.com>
2013-11-10 20:52:50 +0530

    test data for modifyData added to controller

--- Workflow/controller/WorkflowController.js
+++ Workflow/controller/WorkflowController.js
@@ -16,8 +16,25 @@ d3App.controller('WorkflowCtrl', function WorkflowCtrl ($scope, $http) {
     };
 
     $scope.modifyData = function(){
+
         window.rerender = true;
-        $scope.data = window.newData;
+        $scope.data = {
+            "name" : "A",
+            "children" :[
+
+                {"name" : "A2" },
+                {"name" : "A3",
+                    "children":[
+                        {
+                            "name" : "A31",
+                            "children" :[
+                                {"name" : "A311" },{"name" : "A312" },{"name" : "A313" },{"name" : "A314" }, {"name" : "A315" }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        };
     };
 
     $scope.data =  {

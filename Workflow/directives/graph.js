ff58be40f393c06f6da78e36a6a469509cd3d90c
jananij <janani.j@imaginea.com>
2013-11-11 11:58:21 +0530

    render and reDraw clubbed into one method, it varies based on a reDraw boolean

--- Workflow/directives/graph.js
+++ Workflow/directives/graph.js
@@ -4,6 +4,7 @@ d3App.directive('ghVisualization', function () {
     var margin = 20,
         width = 960,
         height = 500 - .5 - margin,
+
         plotCircle = function (group) {
             group.append("svg:circle")
                 .attr("r", 3.5);
@@ -26,7 +27,6 @@ d3App.directive('ghVisualization', function () {
 
         link: function (scope, element, attrs) {
             // set up initial svg object
-
             scope.svgContainer = d3.select('.treeContainer')
                 .append("svg:svg")
                 .attr("width", 400)
@@ -34,100 +34,86 @@ d3App.directive('ghVisualization', function () {
                 .append("svg:g")
                 .attr("transform", "translate(40, 0)");
 
+
             scope.$watch('val', function (newVal, oldVal) {
+                var reDraw = false;
                 if(scope.tree){
-                    scope.reDraw(newVal);
-                } else {
-                    scope.render(newVal, oldVal);
+                    reDraw = true;
                 }
+                scope.render(newVal, oldVal, reDraw)
             });
 
-            scope.reDraw =  function(newVal){
-                // Preparing the data for the tree layout, convert data into an array of nodes
-                var nodes = scope.tree.nodes(newVal),
-                    links = scope.tree.links(nodes),
-                    link = scope.svgContainer.selectAll("path").data(links), node, g;
 
-                link.enter().insert("path","g")
-                    //.attr("transform", "translate(1,1)rotate(0)")
-                    .attr("class", "link")
-                    .attr("d", scope.elbow)
-
-                link.transition()
-                    .duration(50)
-                    //.attr("transform", "translate(1,1)rotate(0)")
-                    .attr("d", scope.elbow)
-
-                link.exit()
-                    .transition()
-                    .duration(50)
-                    .attr("d", scope.elbow)
-                    .remove();
-
-                node = scope.svgContainer.selectAll("g")
-                    .data(nodes, function(d) { return d.name;})
-                    .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
-                    .attr("class", "node")
-
-                g = node.enter().append("g")
-
-                plotCircle(g);
-
-                node.transition()
-                    .duration(50)
-                    .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
-                    .attr("class", "node")
-
-                node.exit()
-                    .transition()
-                    .duration(50)
-                    .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
-                    .remove();
-
-                // place the name atribute left or right depending if children
-
-            };
-
-            scope.render =  function(newVal, oldVal){
+            scope.render =  function(newVal, oldVal, reDraw){
                 if(!newVal){
                     return;
                 }
+                if(! scope.tree){
+                    scope.tree = d3.layout.tree()
+                        .size([300,150]);
+                }
 
-                scope.tree = d3.layout.tree()
-                    .size([300,150]);
+                // Preparing the data for the tree layout, convert data into an array of nodes and create an array with all the links
+                var nodes = scope.tree.nodes(newVal),
+                    links = scope.tree.links(nodes),
+                    link = scope.svgContainer.selectAll("path").data(links), node, g, elbow;
 
-                // Preparing the data for the tree layout, convert data into an array of nodes
-                var nodes = scope.tree.nodes(newVal);
-                // Create an array with all the links
-                var links = scope.tree.links(nodes);
-
-                scope.elbow = function elbow(d, i) {
+                elbow = function elbow(d, i) {
                     return "M" + d.source.y + "," + d.source.x
                         + "H" + d.target.y + "V" + d.target.x ;
                     //+ (d.target.children ? "" : "h" + margin.right);
                 }
 
-                var link = scope.svgContainer.selectAll("path")
-                    .data(links)
-
                 // Enter
-                link.enter().append("path")
+                link.enter().insert("path","g")
                     .attr("class", "link")
-                    .attr("d", scope.elbow);
-                // Exit
-                link.exit().remove();
+                    .attr("d", elbow);
 
-                var node = scope.svgContainer.selectAll("g")
+                if(reDraw){  //TODO 1
+                    link.transition()
+                        .duration(50)
+                        //.attr("transform", "translate(1,1)rotate(0)")
+                        .attr("d", elbow)
+                }
+                // Exit
+                if(reDraw){//TODO 2
+                    link.exit()
+                        .transition()
+                        .duration(50)
+                        .attr("d", elbow)
+                        .remove();
+                }
+                else{
+                    link.exit().remove();
+                }
+
+                node = scope.svgContainer.selectAll("g")
                     .data(nodes, function(d) { return d.name;})
 
                 // Enter
-                node.enter().append("g")
+                g = node.enter().append("g")
                     .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
                     .attr("class", "node")
-                // Exit
-                node.exit().remove();
+                plotCircle(g);
 
-                plotCircle(node);
+                if(reDraw){  //TODO 3
+                    node.transition()
+                        .duration(50)
+                        .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+                        .attr("class", "node")
+                }
+
+                // Exit
+                if(reDraw){ //TODO 4
+                    node.exit()
+                        .transition()
+                        .duration(50)
+                        .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+                        .remove();
+                }
+                else{
+                    node.exit().remove();
+                }
             }
         }
     }

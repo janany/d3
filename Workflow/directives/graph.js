8d73a2742465bff06e12db3ecbbe8fa67fb33ac5
jananij <janani.j@imaginea.com>
2013-11-13 23:19:46 +0530

    Added width,height,scroll to tree container and removed unused code

--- Workflow/directives/graph.js
+++ Workflow/directives/graph.js
@@ -1,15 +1,6 @@
 
 d3App.directive('ghVisualization', function () {
 
-    var margin = {top: 20, right: 20, bottom: 20, left: 20},
-        padding = {top: 60, right: 60, bottom: 60, left: 60},
-        outerWidth = 960,
-        outerHeight = 500,
-        innerWidth = outerWidth - margin.left - margin.right,
-        innerHeight = outerHeight - margin.top - margin.bottom,
-        width = innerWidth - padding.left - padding.right,
-        height = innerHeight - padding.top - padding.bottom;
-
         plotCircle = function (group) {
             group.append("svg:path")
                 .attr("d", function(d){
@@ -69,7 +60,6 @@ d3App.directive('ghVisualization', function () {
                 //.attr("height", outerHeight)
                 .append("svg:g")
                 //.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
 
             scope.$watch('val', function (newVal, oldVal) {
                 var reDraw = false;
